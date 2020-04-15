e557729a5c73784ffb65107be9c48dd2c60f9d43
Nathan Cowley <nathancowley12@gmail.com>
2020-04-15 17:02:23 -0600

    was able to get the title, date, photo, and explanation to show up on the page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import Header from './Header/Header.js'
+import Image from './Header/image.js'
+
+// const url = "https://api.nasa.gov/planetary/apod";
+// const api_key = "sYxJi14Zx77GNtuj82t3ltfigLHWSP8O5VeE0fry"
+
+
 
 function App() {
+const [nasaData, setNasaData] = useState([])
+
+
+
+
+useEffect(() => {
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=sYxJi14Zx77GNtuj82t3ltfigLHWSP8O5VeE0fry')
+  .then(res => {
+    console.log(res)
+    setNasaData(res.data);
+    
+  });
+  
+}, []);
+
+// console.log(nasaData);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <div className="top_content">
+        <Header title={nasaData.title} date={nasaData.date} />
+        <div>
+          <Image  hdurl={nasaData.hdurl}/>
+        </div>
+        </div>
+        <div className="bottom_content">
+          <p className='explanation'>
+            {nasaData.explanation};
+          </p>
+        </div>
     </div>
   );
 }
---
+++ src/Header/image.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Image(props) {
+    return(
+        
+    <div>
+        <img src={props.hdurl}/>
+    </div>
+    )
+}
+
+
+export default Image ;
