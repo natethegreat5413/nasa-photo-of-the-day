6897892e5545b3c3261e16b2a8231414fb3add3f
Nathan Cowley <nathancowley12@gmail.com>
2020-05-13 16:47:52 -0600

    small changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './Header/Header'
 import Middle from './MiddleContent/Middle'
 import Footer from './Footer/Footer'
 
-const url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
+// const url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
 
 function App() {
   const [nasaData, setNasaData] = useState([])
@@ -40,51 +40,4 @@ function App() {
 export default App;
 
 
-// import React, { useState, useEffect } from "react";
-// import "./App.css";
-// import axios from "axios";
-// import Header from './Header/Header.js'
-// import Image from './Header/image.js'
 
-
-// // const url = "https://api.nasa.gov/planetary/apod";
-// // const api_key = "sYxJi14Zx77GNtuj82t3ltfigLHWSP8O5VeE0fry"
-
-
-
-// function App() {
-// const [nasaData, setNasaData] = useState([])
-
-
-
-
-// useEffect(() => {
-//   axios.get('https://api.nasa.gov/planetary/apod?api_key=sYxJi14Zx77GNtuj82t3ltfigLHWSP8O5VeE0fry')
-//   .then(res => {
-//     console.log(res)
-//     setNasaData(res.data);
-    
-//   });
-  
-// }, []);
-
-// // console.log(nasaData);
-
-//   return (
-//     <div className="App">
-//       <div className="top_content">
-//         <Header title={nasaData.title} date={nasaData.date} />
-//         <div>
-//           <Image  hdurl={nasaData.hdurl}/>
-//         </div>
-//         </div>
-//         <div className="bottom_content">
-//           <p className='explanation'>
-//             {nasaData.explanation};
-//           </p>
-//         </div>
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -14,22 +14,3 @@ function Header(props){
 export default Header
 
 
-// import React from 'react';
-// import styled from 'styled-components'
-
-// const WrapperDiv = styled.h1`
-//     color: yellow;
-// `
-
-
-// function Header(props) {
-    
-//     return (
-//         <WrapperDiv>
-//             <h1 className="title">{props.title}</h1>
-//             <h3 className="date">{props.date}</h3>
-//         </WrapperDiv>
-//     )
-// }
-
-// export default Header;
